1bfec8d9eeea0b4a5c14e41c14d6fc229c62c2da
Hanzhen Yi <hzyi@uw.edu>
2019-07-20 19:19:19 -0700

    wip

--- jplab-core/src/main/java/com/hzyi/jplab/core/model/ComponentState.java
+++ jplab-core/src/main/java/com/hzyi/jplab/core/model/ComponentState.java
@@ -3,67 +3,29 @@ package com.hzyi.jplab.core.model;
 import com.google.common.base.Preconditions;
 
 import com.hzyi.jplab.core.util.Buildable;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 /** The dynamic state of a {@code Component} */
-public class ComponentState implements Buildable {
+public class ComponentState {
 
   private final Component component;
-  private final Map<Field, Double> fields;
+  private final Map<Field, Double> fieldMap;
+
+  public ComponentState(Component component) {
+    this.component = component;
+    this.fieldMap = new EnumMap<>();
+  }
 
   public Component getComponent() {
     return this.component;
   }
 
-  public double get(Field field) {
-    return fields.get(field);
+  public Double get(Field field) {
+    return fieldMap.get(field);
   }
 
-  public void set(Field field, double value) {
-    if (!fields.containsKey(field)) {
-      throw new IllegalArgumentException("Field " + field + " does not exist.");
-    }
-    fields.put(field, value);
+  public Double put(Field field, double value) {
+    return fieldMap.put(field, value);
   }
-
-  private ComponentState(Builder builder) {
-    this.component = builder.component;
-    this.fields = builder.fields;
-  }
-
-  public static Builder newBuilder() {
-    return new Builder();
-  }
-
-  public static final class Builder implements 
-      com.hzyi.jplab.core.util.Builder<Builder> {
-
-    private Component component;
-    private final Map<Field, Double> fields = new HashMap();
-
-    public Builder setComponent(Component component) {
-      this.component = component;
-      return this;
-    }
-
-    /**
-     * This method sets the {@code Component.FIELD} to {@code value}.
-     * If the {@code Component.FIELD} was set before, this method 
-     * overrides it.
-     */
-    public Builder set(Field field, double value) {
-      fields.put(field, value);
-      return this;
-    }
-
-    @Override
-    public ComponentState build() {
-      Preconditions.checkNotNull(component, "component can't be null.");
-      return new ComponentState(this);
-    }
- 
-  }
-
-
 }
--- jplab-core/src/main/java/com/hzyi/jplab/core/model/Field.java
+++ jplab-core/src/main/java/com/hzyi/jplab/core/model/Field.java
@@ -1,52 +1,5 @@
 package com.hzyi.jplab.core.model;
 
-import java.util.Map;
-import java.util.HashMap;
-
-public class Field {
-
-  private static final Map<String, Field> fields = new HashMap<>();
-
-  private final String name;
-
-  public static final Field LOCX = add("loc_x");
-  public static final Field LOCY = add("loc_y");
-  public static final Field LOC = add("loc");
-  public static final Field DIRX = add("dir_x");
-  public static final Field DIRY = add("dir_y");
-  public static final Field DIR = add("dir");
-  public static final Field VX = add("v_x");
-  public static final Field VY = add("v_y");
-  public static final Field V = add("v");
-  public static final Field OMEGA = add("omega");
-  public static final Field AX = add("a_x");
-  public static final Field AY = add("a_y");
-  public static final Field ALPHA = add("alpha");
-  public static final Field A = add("a");
-
-
-  private Field(String name) {
-    this.name = name;
-  }
-
-  static Field of(String name) {
-    Field field = fields.get(name);
-    if (field == null) {
-      throw new IllegalArgumentException("Field with the name of " + name + " does not exist.");
-    }
-    return field;
-  }
-
-  static Field add(String name) {
-    if (fields.containsKey(name)) {
-      throw new IllegalArgumentException("Field with the name of " + name + " already exists.");
-    }
-    Field field = new Field(name);
-    fields.put(name, field);
-    return field;
-  }
-
-  static void reset() {
-    fields.clear();
-  }
+public enum Field {
+  X, Y, DIRX, DIRY, VX, VY, OMEGA, AX, AY, ALPHA
 }
