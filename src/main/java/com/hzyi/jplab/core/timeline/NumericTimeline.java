3a5c151910a4d97006bf1841d0121e5397c1ca93
Hanzhen Yi <hzyi@uw.edu>
2020-03-08 22:03:44 -0700

    wip

---
+++ src/main/java/com/hzyi/jplab/core/timeline/NumericTimeline.java
@@ -0,0 +1,15 @@
+package com.hzyi.jplab.core.timeline;
+
+import com.hzyi.jplab.core.model.AssemblySnapshot;
+import java.util.function.BiFunction;
+
+public class NumericTimeline implements Timeline {
+
+  private final double timeStep;
+
+  public NumericTimeline(double timeStep) {
+    
+  }
+
+
+}
